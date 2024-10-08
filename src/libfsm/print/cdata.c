7b7c9f322ac99bc25ebf4268f4950210d0e5d532
Scott Vokes <svokes@fastly.com>
2024-10-08 15:25:37 -0400

    WIP bugfix #2: check the potential match is terminated.

--- src/libfsm/print/cdata.c
+++ src/libfsm/print/cdata.c
@@ -597,11 +597,6 @@ save_state_endids(struct cdata_config *config, const struct ir_state_endids *end
 		return true;
 	}
 
-	/* Special case: if the set is [0], don't reuse a bare 0 unless it
-	 * has a 0 terminator after, because otherwise that offset will be
-	 * unintentionally combined with the next values. */
-	const bool single_0 = endids->count == 1 && endids->ids[0] == 0;
-
 #if REUSE_ENDID_SETS
 	/* Intern the run of endids. They are often identical
 	 * between states, so the earlier reference could be reused.
@@ -623,13 +618,14 @@ save_state_endids(struct cdata_config *config, const struct ir_state_endids *end
 				break; /* mismatch */
 			}
 		}
-		if (e_i == endids->count) { /* got a match */
-			if (!single_0 ||
-			    /* the 0 has a 0 terminator */
-			    (b_i + 2 < config->endid_buf.used && config->endid_buf.buf[b_i + 2] == 0)) {
-				naive_offset = b_i;
-				break;
-			}
+
+		/* If there's a potential match, check that it isn't followed by
+		 * a value > the last, because it would falsely continue the run. */
+		if (e_i == endids->count
+		    && b_i + e_i + 1 < config->endid_buf.used
+		    && config->endid_buf.buf[b_i + e_i + 1] <= endids->ids[e_i - 1]) {
+			naive_offset = b_i;
+			break;
 		}
 	}
 
@@ -704,9 +700,6 @@ save_state_eager_outputs(struct cdata_config *config, const struct ir_state_eage
 		return true;
 	}
 
-	/* See single_0 in save_state_endids. */
-	const bool single_0 = eager_outputs->count == 1 && eager_outputs->ids[0] == 0;
-
 #if REUSE_EAGER_OUTPUT_SETS
 #if REUSE_NAIVE || EXPENSIVE_CHECKS
 	/* Linear scan. See comments about reuse in save_state_endids. */
@@ -721,13 +714,14 @@ save_state_eager_outputs(struct cdata_config *config, const struct ir_state_eage
 				break; /* mismatch */
 			}
 		}
-		if (e_i == eager_outputs->count) { /* got a match */
-			if (!single_0 ||
-			    /* the 0 has a 0 terminator */
-			    (b_i + 2 < config->endid_buf.used && config->endid_buf.buf[b_i + 2] == 0)) {
-				naive_offset = b_i;
-				break;
-			}
+
+		/* If there's a potential match, check that it isn't followed by
+		 * a value > the last, because it would falsely continue the run. */
+		if (e_i == eager_outputs->count
+		    && b_i + e_i + 1 < config->eager_output_buf.used
+		    && config->eager_output_buf.buf[b_i + e_i + 1] <= eager_outputs->ids[e_i - 1]) {
+			naive_offset = b_i;
+			break;
 		}
 	}
 
